6ce212b1f2eb7c793d7d355c9cd0431361ce2dd0
Sergio <s.garcianortes@gmail.com>
2022-01-18 00:31:10 +0100

    mod

--- test/test.js
+++ test/test.js
@@ -2,27 +2,5 @@
 //adios 2
 //adios 3
 //adios 4
-//jj
-<<<<<<< Updated upstream
-//kk
-//ll
-//zz
-//lili
-//zz
-//ss
-//zz
-//jj
-//ss
-//hh
-//zz
-//jj
-//ss
-//zz
-//jj
-//ss
-=======
-//zz
-//jj
-//ss
-//hh
->>>>>>> Stashed changes
+
+
